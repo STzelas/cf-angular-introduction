9652104697e4524fe0bbac1622387c2f06a098ef
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-24 18:02:52 +0300

    feat: app component and import of person table component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Sotiris'
+
+  person = {
+    givenName: "Sotiris",
+    surname: "Tzelatidis",
+    age: 27,
+    email: 'stzelati@aueb.gr'
+  }
 }
