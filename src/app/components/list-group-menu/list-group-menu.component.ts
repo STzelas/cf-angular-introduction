1cc3440d0d766b6dd9bd1085e09c5e737f13ff85
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-29 19:21:53 +0300

    feat: new fields to the list (Simple datatable Example)

--- src/app/components/list-group-menu/list-group-menu.component.ts
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -11,6 +11,7 @@ export class ListGroupMenuComponent {
   menu = [
     { text: 'Component Input Example', linkName: 'component-input-example' },
     { text: '@for Directive Example', linkName: 'for-directive-example'},
-    { text: 'Event Bind Example', linkName: 'event-bind-example'}
+    { text: 'Event Bind Example', linkName: 'event-bind-example'},
+    { text: 'Simple DataTable Example', linkName: 'simple-datatable-example'}
   ]
 }
