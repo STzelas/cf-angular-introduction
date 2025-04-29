fe3c300d5104267459836df0ca5ef5ba422dfeb3
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-29 18:22:05 +0300

    feat: list menu component created and added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { PersonTableComponent } from './components/person-table/person-table.component';
-import { Person } from './shared/interfaces/person';
-import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],  // [PersonTableComponent, EventBindExampleComponent] / δεν τα χρησιμοποιούμε προς το παρόν
+  imports: [RouterOutlet, RouterLink, ListGroupMenuComponent],  // [PersonTableComponent, EventBindExampleComponent] / δεν τα χρησιμοποιούμε προς το παρόν
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
