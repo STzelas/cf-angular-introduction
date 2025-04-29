9dce8cf0cb4bca44d37ff3349f64e44628dd6990
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-29 17:04:16 +0300

    feat: person0 and 1 variable based on Person interface created

--- src/app/components/component-input-example/component-input-example.component.ts
+++ src/app/components/component-input-example/component-input-example.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-component-input-example',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './component-input-example.component.css'
 })
 export class ComponentInputExampleComponent {
+  person0: Person = {
+    givenName: 'Xristos',
+    surname: "Fragkos",
+    age: 30,
+    email: "cxristos@aueb.gr",
+    address: "New York, USA",
+  }
 
+  person1: Person = {
+    givenName: 'John',
+    surname: 'Doe',
+    age: 0,
+    email: 'john@aueb.gr',
+    address: 'Athens, Greece'
+  }
 }
