f1f43ff1f497a1a20257153f97ae329b84bedc55
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-30 19:30:27 +0300

    feat: material libs imported to new component

--- src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
+++ src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms'
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-eperson-template-driven-form',
-  imports: [],
+  imports: [FormsModule, MatSelectModule, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './eperson-template-driven-form.component.html',
   styleUrl: './eperson-template-driven-form.component.css'
 })
