d31aafc6e2da08bf3561d513e5c0ed9fc4c10939
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-30 17:29:07 +0300

    feat: functions sortData for ascending and descending sort and sortSign for sorting icon created

--- src/app/components/simple-data-table/simple-data-table.component.ts
+++ src/app/components/simple-data-table/simple-data-table.component.ts
@@ -19,8 +19,32 @@ export class SimpleDataTableComponent {
     education: 'none'
   }
 
-  sortData(sortKey: keyof EPerson) {
+  // Θα βάλει sort στην λίστα ανάλογα το πεδίο (key) που πατάμε πχ givenName
+  sortData(sortKey: keyof EPerson): void { // Δεν  θα επιστρέψει κάτι απλά θα κάνει sort
     console.log(sortKey)
+    if(this.sortOrder[sortKey] === 'asc') {
+      this.sortOrder[sortKey] = 'desc'
+      this.data = sortBy(this.data, sortKey).reverse()
+    } else {
+      this.sortOrder[sortKey] = 'asc'
+      this.data = sortBy(this.data, sortKey) // μόνο asc τα κάνει η sortBy()
+    }
+    
+    for (let key in this.sortOrder) {
+      if(key !== sortKey) {
+        this.sortOrder[key as keyof EPerson] = 'none';
+      }
+    }
+    console.log(this.sortOrder)
+  }
+
+  sortSign(sortKey: keyof EPerson): string {
+    if(this.sortOrder[sortKey] === 'asc') {
+      return '\u2191'
+    } else if (this.sortOrder[sortKey] === 'desc') {
+      return '\u2193'
+    } else
+    return ''
   }
 
   onPersonClicked(person: EPerson) {
