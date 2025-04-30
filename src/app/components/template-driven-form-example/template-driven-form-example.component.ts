8b82bbaa394f9223ef3eea79366a2debe3ffd4a7
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-30 19:31:47 +0300

    feat: component html created with imported components

--- src/app/components/template-driven-form-example/template-driven-form-example.component.ts
+++ src/app/components/template-driven-form-example/template-driven-form-example.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { EpersonTemplateDrivenFormComponent } from '../eperson-template-driven-form/eperson-template-driven-form.component';
+import { PersonTableComponent } from '../person-table/person-table.component';
+import { SimpleDataTableComponent } from '../simple-data-table/simple-data-table.component';
 
 @Component({
   selector: 'app-template-driven-form-example',
-  imports: [],
+  imports: [EpersonTemplateDrivenFormComponent, PersonTableComponent, SimpleDataTableComponent],
   templateUrl: './template-driven-form-example.component.html',
   styleUrl: './template-driven-form-example.component.css'
 })
