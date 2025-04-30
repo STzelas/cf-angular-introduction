3d7629b9677e55d17b42499e61346bbe4429f1c5
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-30 19:09:00 +0300

    feat: new components generated

---
+++ src/app/components/template-driven-form-example/template-driven-form-example.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form-example',
+  imports: [],
+  templateUrl: './template-driven-form-example.component.html',
+  styleUrl: './template-driven-form-example.component.css'
+})
+export class TemplateDrivenFormExampleComponent {
+
+}
