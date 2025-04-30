ea96a2fb041b81f31ba746b039f73ce4d25b72b8
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-30 19:30:46 +0300

    feat: routes added for new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { ComponentInputExampleComponent } from './components/component-input-exa
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
+import { TemplateDrivenFormExampleComponent } from './components/template-driven-form-example/template-driven-form-example.component';
+import { EpersonTemplateDrivenFormComponent } from './components/eperson-template-driven-form/eperson-template-driven-form.component';
 
 export const routes: Routes = [
   {path: "welcome", component: WelcomeComponent},
@@ -12,6 +14,8 @@ export const routes: Routes = [
   {path: 'component-input-example', component: ComponentInputExampleComponent},
   {path: 'event-bind-example', component: EventBindExampleComponent},
   {path: 'simple-datatable-example', component: SimpleDatatableExampleComponent},
+  {path: 'template-driven-form-example', component: TemplateDrivenFormExampleComponent},
+  {path: 'eperson-template-driven-form', component: EpersonTemplateDrivenFormComponent},
   {path: 'component-output-example', component: ComponentOutputExampleComponent},
   {path: '', redirectTo:'/welcome', pathMatch:'full'}
 ];
