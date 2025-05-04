01ededd3632b46f9919f63eb73b9ca11e1371188
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-04 19:07:33 +0300

    feat: isPerson and isEPerson function created to check personInput type, to show address or education accordingly on template

--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
 import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
@@ -8,13 +9,30 @@ import { Person } from 'src/app/shared/interfaces/person';
   styleUrl: './person-table.component.css'
 })
 export class PersonTableComponent {
-  @Input() personInput: Person | undefined // Ή θα πληρεί τα κριτήρια του interface ή δεν θα στείλει τιποτα
+  @Input() personInput: Person | EPerson | undefined // Ή θα πληρεί τα κριτήρια του interface ή δεν θα στείλει τιποτα
   name = "Thanassis"
+  addressOrEducation = ""
 
   person = {
     givenName:"Thanasis",
     surname: "Androutsos",
     age: 20,
     email: "thanasis@aueb.gr"
+  }
+
+  isPerson():boolean {
+    if(this.personInput && 'address' in this.personInput) {
+      this.addressOrEducation = this.personInput.address
+      return 'address' in this.personInput
+    }
+    return false
+  }
+
+  isEPerson():boolean {
+    if(this.personInput && 'education' in this.personInput) {
+      this.addressOrEducation = this.personInput.education
+      return 'education' in this.personInput
+    }
+    return false
   }
 }
