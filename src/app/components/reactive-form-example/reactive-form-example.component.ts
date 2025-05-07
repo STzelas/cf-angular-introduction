bb58843df1d4f6ff7dc77b61d43fc7a8b04f4267
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-07 19:10:31 +0300

    space

--- src/app/components/reactive-form-example/reactive-form-example.component.ts
+++ src/app/components/reactive-form-example/reactive-form-example.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EpersonReactiveFormComponent } from '../eperson-reactive-form/eperson-reactive-form.component';
+import { PersonTableComponent } from '../person-table/person-table.component';
+import { SimpleDataTableComponent } from '../simple-data-table/simple-data-table.component';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
 
 @Component({
   selector: 'app-reactive-form-example',
-  imports: [],
+  imports: [EpersonReactiveFormComponent, PersonTableComponent, SimpleDataTableComponent],
   templateUrl: './reactive-form-example.component.html',
   styleUrl: './reactive-form-example.component.css'
 })
 export class ReactiveFormExampleComponent {
-
-}
+  @Output() personInput2: EPerson | undefined
+} 
