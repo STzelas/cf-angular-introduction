36819a9c8b977542780e72827989384aabe1b5bd
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-09 18:15:00 +0300

    feat: new service for jokes created with getDadJokes and getChuchNorrisJoke to get jokes from the corresponded url

---
+++ src/app/shared/services/jokes.service.ts
@@ -0,0 +1,31 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DadJoke, ChuckNorrisJoke } from '../interfaces/joke';
+
+const DAD_JOKES_API_URL = 'https://icanhazdadjoke.com/'
+const CHUCK_NORRIS_JOKES_API_URL = 'https://api.chucknorris.io/'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JokesService {
+  http: HttpClient = inject(HttpClient)
+
+  constructor() {}
+
+  getDadJokes() {
+    return this.http.get<DadJoke>(DAD_JOKES_API_URL, {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+  }
+
+  getChuchNorrisJoke() {
+    return this.http.get<ChuckNorrisJoke>(CHUCK_NORRIS_JOKES_API_URL, {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+  }
+}
