4c3d09e82e4c15b4951c365b6a5c9246112554d6
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-09 19:35:07 +0300

    feat: new functions for http client component, to request jokes from dad and chuck norris APIs added on template and on ngOnInit

--- src/app/components/http-client-example/http-client-example.component.ts
+++ src/app/components/http-client-example/http-client-example.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { JokesService } from 'src/app/shared/services/jokes.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DadJoke } from 'src/app/shared/interfaces/joke';
 
 @Component({
   selector: 'app-http-client-example',
-  imports: [],
+  imports: [ MatCardModule, MatButtonModule],
   templateUrl: './http-client-example.component.html',
   styleUrl: './http-client-example.component.css'
 })
-export class HttpClientExampleComponent {
+export class HttpClientExampleComponent implements OnInit {
+  jokesService = inject(JokesService)
+  public dadJoke: string = ""
+  public chuckNorrisJoke: string = ""
 
+  /**
+   * Το request του getDadJokes είναι promise
+   * Όταν θα τρέξει θα μου επιστρέψει κάτι
+   * για αυτό πρέπει να γράψω .subscribe
+   */
+  ngOnInit(): void {
+    // this.jokesService
+    // .getDadJokes()
+    // .subscribe((data) => {
+    //   console.log("DAD JOKE", data)
+    //   console.log("Dad just Joke", data.joke)
+    // })
+
+    // this.jokesService
+    // .getChuchNorrisJoke()
+    // .subscribe((data) => {
+    //   console.log("Chuck Norris JOKE", data)
+    //   console.log("Chuck just Joke", data.value)
+    // })
+
+    this.refreshChuckNorrisJoke()
+    this.refreshDadJoke()
+  }
+
+  refreshDadJoke() {
+    this.jokesService.getDadJokes()
+        .subscribe((data) => {
+          // console.log("New dad Joke -> ", data.joke)
+          this.dadJoke = data.joke
+        })
+  }
+
+  refreshChuckNorrisJoke() {
+    this.jokesService.getChuchNorrisJoke()
+        .subscribe((data) => {
+          // console.log("New ChuckNorris Joke -> ", data.value)
+          this.chuckNorrisJoke = data.value
+        })
+  }
 }
