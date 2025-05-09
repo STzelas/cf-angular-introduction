7d40adeb102b80f5c84433dc3ac143eab2f29d8f
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-09 17:21:44 +0300

    feat: .pop removed for array and added spread operator

--- src/app/components/reactive-form-example/reactive-form-example.component.ts
+++ src/app/components/reactive-form-example/reactive-form-example.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { EpersonReactiveFormComponent } from '../eperson-reactive-form/eperson-reactive-form.component';
 import { PersonTableComponent } from '../person-table/person-table.component';
 import { SimpleDataTableComponent } from '../simple-data-table/simple-data-table.component';
 import { EPerson } from 'src/app/shared/interfaces/eperson';
-import { PersonService } from 'src/app/shared/services/person.service';
+// import { PersonService } from 'src/app/shared/services/person.service';
 
 @Component({
   selector: 'app-reactive-form-example',
@@ -12,14 +12,14 @@ import { PersonService } from 'src/app/shared/services/person.service';
   styleUrl: './reactive-form-example.component.css'
 })
 export class ReactiveFormExampleComponent {
-  personService = inject(PersonService)
+  // personService = inject(PersonService)
   currentPerson: EPerson | undefined
   persons: EPerson[] = []
 
   onPerson(data: EPerson) {
     // console.log("Father >> ", data)
     this.currentPerson = data
-    this.persons.push(data)
-    this.personService.modifiedDataTable.set(true)
+    this.persons = [...this.persons, data]
+    // this.personService.modifiedDataTable.set(true)
   }
 } 
