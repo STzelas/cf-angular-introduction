64be5ddd922db9f18b3c92a63861bf3afc098673
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-14 17:18:26 +0300

    feat: loginUser function created

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,14 +2,20 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 import { User } from '../interfaces/user';
+import { Credentials } from '../interfaces/auth';
 
 const API_URL = `${environment.apiURL}/api/users`
+const API_URL_AUTH = `${environment.apiURL}/api/auth`
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   http: HttpClient = inject(HttpClient)
+
+  loginUser(credentials: Credentials) {
+    return this.http.post<{status: boolean, data: string}>(`${API_URL_AUTH}/login`, credentials)
+  }
 
   registerUser(user: User) {
     return this.http.post<{status: boolean}>(`${API_URL}`, user)
@@ -22,4 +28,6 @@ export class UserService {
   check_duplicate_username(username: string) {
     return this.http.get<{status: boolean}>(`${API_URL}/check_duplicate_username/${username}`)
   }
+
+
 }
