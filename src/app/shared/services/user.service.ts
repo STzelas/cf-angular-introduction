7f4bbea2dbe8fd5b79b859e0c7c3848cfc35a64d
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-14 19:23:20 +0300

    feat: signal and effect added, logoutUser function added

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal, effect } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
-import { User } from '../interfaces/user';
+import { LoggedInUser, User } from '../interfaces/user';
 import { Credentials } from '../interfaces/auth';
+import { Router } from '@angular/router';
 
 const API_URL = `${environment.apiURL}/api/users`
 const API_URL_AUTH = `${environment.apiURL}/api/auth`
@@ -12,9 +13,28 @@ const API_URL_AUTH = `${environment.apiURL}/api/auth`
 })
 export class UserService {
   http: HttpClient = inject(HttpClient)
+  router = inject(Router)
+
+  user$ = signal<LoggedInUser | null>(null)
+
+  constructor() {
+    effect(() => {
+      if (this.user$()) {
+        console.log('User Logged in', this.user$()?.username)
+      } else {
+        console.log('No user logged in')
+      }
+    })
+  }
 
   loginUser(credentials: Credentials) {
     return this.http.post<{status: boolean, data: string}>(`${API_URL_AUTH}/login`, credentials)
+  }
+
+  logoutUser() {
+    this.user$.set(null)
+    localStorage.removeItem('access_token')
+    this.router.navigate(['user-login'])
   }
 
   registerUser(user: User) {
