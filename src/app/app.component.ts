6c86b262e8938f9c4114bab99959a5eba8903abf
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-14 19:22:01 +0300

    feat: navbar imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ListGroupMenuComponent],  // [PersonTableComponent, EventBindExampleComponent] / δεν τα χρησιμοποιούμε προς το παρόν
+  imports: [RouterOutlet, ListGroupMenuComponent, NavbarComponent],  // [PersonTableComponent, EventBindExampleComponent] / δεν τα χρησιμοποιούμε προς το παρόν
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
