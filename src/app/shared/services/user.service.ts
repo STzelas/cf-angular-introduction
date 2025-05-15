963093cfc3af288b4b08144fe1945f995fd832aa
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-15 20:07:18 +0300

    feat: constructor created to decode token on init

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment.development';
 import { LoggedInUser, User } from '../interfaces/user';
 import { Credentials } from '../interfaces/auth';
 import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 
 const API_URL = `${environment.apiURL}/api/users`
 const API_URL_AUTH = `${environment.apiURL}/api/auth`
@@ -18,6 +19,15 @@ export class UserService {
   user$ = signal<LoggedInUser | null>(null)
 
   constructor() {
+    const access_token = localStorage.getItem('access_token')
+    if (access_token) {
+      const decodedTokenSubject = jwtDecode(access_token) as unknown as LoggedInUser
+      this.user$.set({
+        username: decodedTokenSubject.username,
+        email: decodedTokenSubject.email,
+        roles: decodedTokenSubject.roles
+      })
+    }
     effect(() => {
       if (this.user$()) {
         console.log('User Logged in', this.user$()?.username)
@@ -49,5 +59,38 @@ export class UserService {
     return this.http.get<{status: boolean}>(`${API_URL}/check_duplicate_username/${username}`)
   }
 
+  isTokenExpired(): boolean {
+    const token = localStorage.getItem('access_token')
 
+    if(!token) 
+      return true
+
+    try {
+      const decoded = jwtDecode(token)
+      const exp = decoded.exp
+      const now = Math.floor(Date.now() / 1000)
+      if (exp) {   
+        return exp < now
+      } else {
+        return true
+      }
+    } catch (err) {
+      return true
+    }
+  }
+
+  redirectToGoogleLogin() {
+    const clientId = '825567864650-phqsstcu1t8jkah97q8d8r3100htgsfa.apps.googleusercontent.com'
+    const redirect_uri = 'http://localhost:3000/api/auth/google/callback'
+    const scope = 'email profile'
+    const responseType = 'code'
+    const accessType = 'offline'
+
+    const url = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirect_uri}&response_type=${responseType}&scope=${scope}&access_type=${accessType}`
+
+    window.location.href = url
+  }
 }
+
+
+// https://accounts.google.com/o/oauth2/auth?client_id=825567864650-phqsstcu1t8jkah97q8d8r3100htgsfa.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
