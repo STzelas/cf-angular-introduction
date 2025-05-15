fcee47c1e62b77da666f566d60a5823991080d41
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-15 20:06:04 +0300

    feat: new ngOnInit function added to get token from url and save it to local storage

--- src/app/components/user-login/user-login.component.ts
+++ src/app/components/user-login/user-login.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from 'src/app/shared/services/user.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Credentials } from 'src/app/shared/interfaces/auth';
 import { jwtDecode } from 'jwt-decode';
 import { LoggedInUser } from 'src/app/shared/interfaces/user';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
@@ -12,9 +12,30 @@ import { Router } from '@angular/router';
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
-export class UserLoginComponent {
+export class UserLoginComponent implements OnInit {
+
+  
   userService = inject(UserService)
   router = inject(Router)
+  route = inject(ActivatedRoute)
+
+  ngOnInit(): void {
+    this.route.queryParams
+    .subscribe(params => {
+      const access_token = params['token']
+      if (access_token) {
+        localStorage.setItem('access_token', access_token)
+        const decodedTokenSubject = jwtDecode(access_token) as unknown as LoggedInUser
+
+        this.userService.user$.set({
+          username: decodedTokenSubject.username,
+          email: decodedTokenSubject.email,
+          roles: decodedTokenSubject.roles
+        })
+        this.router.navigate(['user-registration'])
+      }
+    })
+  }
 
   form = new FormGroup({
     username: new FormControl('', Validators.required),
@@ -45,5 +66,10 @@ export class UserLoginComponent {
           console.log("Not logged in", error)
         }
     })
+  }
+
+  googleLogin() {
+    this.userService.redirectToGoogleLogin()
+
   }
 }
